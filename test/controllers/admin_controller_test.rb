e114235585fd6dedbb9fdbbfe6547cd8bdc9fa5f
Kinnari <kasaksharma15@gmail.com>
2018-01-16 23:06:34 +0530

    Reduce files

--- test/controllers/admin_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get admin_path
-    assert_response :success
-  end
-
-end
--- test/models/jobtype_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class JobtypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
